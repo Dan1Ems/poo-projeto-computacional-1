6c3f07d3a3f5e89a7b304caae1e1d082cc607061
Daniels <daniel.santospv29@gmail.com>
2026-09-24 16:34:24 -0300

    Movimentação suave entre Nodes [pacman.py]

--- pacman.py
+++ pacman.py
@@ -6,7 +6,6 @@ from constants import *
 class Pacman(object):
     def __init__(self, node):
         self.name = PACMAN
-        #self.position = Vector2(200, 400)
         self.directions = {UP:Vector2(0, -1), DOWN:Vector2(0, 1), LEFT:Vector2(-1, 0), RIGHT:Vector2(1, 0), STOP:Vector2()}
         self.direction = STOP
         self.speed = 100 * TILEWIDTH/16
@@ -14,16 +13,22 @@ class Pacman(object):
         self.color = YELLOW
         self.node = node
         self.setPosition()
+        self.target = node
         
     def setPosition(self):
         self.position = self.node.position.copy()
     
     def update(self, dt):
-        #self.position += self.directions[self.direction]*self.speed*dt
+        self.position += self.directions[self.direction]*self.speed*dt
         direction = self.getValidKey()
-        self.direction = direction
-        self.node = self.getNewTarget(direction)
-        self.setPosition()
+        if self.overshotTarget():
+            self.node = self.target
+            self.target = self.getNewTarget(direction)
+            if self.target is not self.node:
+                self.direction = direction
+            else:
+                self.direction = STOP
+            self.setPosition()
         
     def validDirection(self, direction):
         if direction is not STOP:
@@ -51,3 +56,13 @@ class Pacman(object):
     def render(self, screen):
         p = self.position.asInt()
         pygame.draw.circle(screen, self.color, p, self.radius)
+        
+    def overshotTarget(self):
+        if self.target is not None:
+            vec1 = self.target.position - self.node.position
+            vec2 = self.position - self.node.position
+            node2Target = vec1.magnitudeSquared()
+            node2Self = vec2.magnitudeSquared()
+            return node2Self >= node2Target
+        return False
+            
